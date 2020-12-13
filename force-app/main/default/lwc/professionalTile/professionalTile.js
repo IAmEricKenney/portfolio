3c4fdd3462a18bc17170124b5af1af56d3f0e5d4
Eric Kenney <eric@erickenney.com>
2020-12-13 14:03:26 -0700

    updated code

--- force-app/main/default/lwc/professionalTile/professionalTile.js
+++ force-app/main/default/lwc/professionalTile/professionalTile.js
@@ -1,9 +1,17 @@
-import { LightningElement, api } from 'lwc';
-
+import {
+    LightningElement,
+    api,
+    track
+} from 'lwc';
 export default class ProfessionalTile extends LightningElement {
-    @api professional;
-    get dateRangeString() {
-        const endDateText = this.sObject.To_Present__c = true ? 'Present' : this.sObject.End_Date__c;
-        return '${this.sObject.Start_Date__c} to ${endDateText}';
-     }
-}
+    @track _professional;
+    @api
+    @set professional(value) {
+        _professional = {
+            ...value,
+            dateRangeString: `${value.Start_Date__c} to ${value.To_Present__c ? 'Present' : value.End_Date__c}`
+        }
+        get professional(){
+            return _professional;
+        }
+    }
