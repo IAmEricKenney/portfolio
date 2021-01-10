e4426efcb1653f7b51cddb4621aee7e4a2d86397
Eric Kenney <eric@erickenney.com>
2021-01-10 00:06:18 -0700

    removed unused api import

--- force-app/main/default/lwc/professionalList/professionalList.js
+++ force-app/main/default/lwc/professionalList/professionalList.js
@@ -1,6 +1,6 @@
-import { LightningElement, api, wire } from 'lwc';
-import getAllProfExp from '@salesforce/apex/expController.getAllProfExp';
+import { LightningElement, wire } from "lwc";
+import getAllProfExp from "@salesforce/apex/expController.getAllProfExp";
 
 export default class ProfessionalList extends LightningElement {
-    @wire(getAllProfExp) profexps;
+  @wire(getAllProfExp) profexps;
 }
